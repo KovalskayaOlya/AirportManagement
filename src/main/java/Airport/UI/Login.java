af77b01a36e58e76217cbb82a7dd0dc65f4850f6
kovalskaya <kovalskaya-olechka23@mail.ru>
2017-04-21 19:48:57 +0300

    commit

--- src/main/java/Airport/UI/Login.java
+++ src/main/java/Airport/UI/Login.java
@@ -1,7 +1,15 @@
 package Airport.UI;
 
+import javax.swing.*;
+
 /**
  * Created by Olechka on 21.04.2017.
  */
 public class Login {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+    private JButton button2;
+
+
 }
